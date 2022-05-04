20fade97a5171c4cd70076816ede23b62bfbc20e
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-04 12:26:24 -0700

    Added location routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,12 @@
 // import models
 const Traveller = require('./Traveller');
-const Location = require('./Traveller');
+const Location = require('./Location');
 const Trip = require('./Trips');
 
-//Travells have many locations, and location have many travellers through trips
+//Travellers have many locations
+Traveller.hasMany(Location,{
+    foreignKey:'traveller_id',
+    unique: false
+});
+
+//location have many travellers through trips
--- routes/api/locations-routes.js
+++ routes/api/locations-routes.js
@@ -1,14 +1,50 @@
 const router = require("express").Router();
-const { } = require("../../models");
+const { Location } = require("../../models");
 
 router.get("/", (req, res) => {
+    try{
+        const locationData = Location.findAll({
+        });
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    });
 
-});
+router.get("/:id", (req, res) => {
+    try{
+        const locationData = Location.findByPk(req.params.id, {
+        });
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 router.post("/", (req, res) => {
-
+    try{
+        const newLocation = Location.create({
+            location_id: req.body.location_id,
+        });
+        res.status(200).json(newLocation);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 router.delete("/:id", (req, res) => {
+    try {
+        const locationData= Location.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
 
+        if (!locationData) {
+            res.status(404).json({ message: 'No location found with that ID!'});
+            return
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
