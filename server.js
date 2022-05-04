668bd9b24cd326ffadca9229c4bc7a5f432e0fae
Maung Htike <maunghtike1999@gmail.com>
2022-05-04 11:18:25 -0700

    add base routes

--- server.js
+++ server.js
@@ -9,13 +9,30 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/api/travellers", (req, res) => {
 
+});
+app.post("/api/travellers", (req, res) => {
 
+});
+app.delete("/api/travellers/:id", (req, res) => {
 
+});
+app.get("/api/locations", (req, res) => {
 
+});
+app.post("/api/locations", (req, res) => {
 
+});
+app.delete("/api/locations/:id", (req, res) => {
 
+});
+app.post("/api/trips", (req, res) => {
 
+});
+app.delete("/api/trips/:id", (req, res) => {
+
+});
 
 // listens for the server
 app.listen(PORT, () => {
